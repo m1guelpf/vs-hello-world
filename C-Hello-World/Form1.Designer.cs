19785eb25fab11cdda87c14ad9a1389b08be7124
Miguel Piedrafita <git@miguelpiedrafita.com>
2015-11-09 21:06:05 +0100

    Changes to be committed: new file:   C-Hello-World.sln new file:   C-Hello-World/App.config new file:   C-Hello-World/C Hello World.csproj new file:   C-Hello-World/Form1.Designer.cs new file:   C-Hello-World/Form1.cs new file:   C-Hello-World/Form1.resx new file:   C-Hello-World/Program.cs new file:   C-Hello-World/Properties/AssemblyInfo.cs new file:   C-Hello-World/Properties/Resources.Designer.cs new file:   C-Hello-World/Properties/Resources.resx new file:   C-Hello-World/Properties/Settings.Designer.cs new file:   C-Hello-World/Properties/Settings.settings deleted:    Hello World!.sln deleted:    Hello World!/App.config deleted:    Hello World!/Hello World!.csproj deleted:    Hello World!/Program.cs deleted:    Hello World!/Properties/AssemblyInfo.cs

---
+++ C-Hello-World/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace C_Hello_World
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Algerian", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(50, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(220, 71);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Hello";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Algerian", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(30, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(257, 71);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "World!";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(329, 213);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Hello World";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
